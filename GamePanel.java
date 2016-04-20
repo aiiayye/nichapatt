8bbe08e5d0025208f124c51e1a8db655b546a2ca
nichapatt <anbeer_ayeaye@hotmail.com>
2016-04-21 00:16:44 +0700

    #02 สร้างตัวยาน
    ยานสามารถเลื่อนซ้ายขวาได้

--- GamePanel.java
+++ GamePanel.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -9,12 +10,23 @@ public class GamePanel extends JPanel {
 	
 	private BufferedImage bg;	
 	Graphics2D bg2;
+	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 
 	public GamePanel() {
 		bg = new BufferedImage(400, 610, BufferedImage.TYPE_INT_ARGB);
 		bg2 = (Graphics2D) bg.getGraphics();
 		bg2.setBackground(Color.BLACK);
+		
+	}
+	
+	public void updateGameUI(GameReporter reporter){
 		bg2.clearRect(0, 0, 400, 610);
+		bg2.setColor(Color.WHITE);		
+		for(Sprite s : sprites){
+			s.draw(bg2);
+		}
+		
+		repaint();
 	}
 
 
