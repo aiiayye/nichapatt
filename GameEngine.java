ae8e4e1ff63498317ed10f8c9a83c91883da3bc1
nichapatt <anbeer_ayeaye@hotmail.com>
2016-04-22 20:25:20 +0700

    #03 สร้างดาวให้ตกลงมา
    ดาวตกลงมาแบบrandom

--- GameEngine.java
+++ GameEngine.java
@@ -14,6 +14,8 @@ public class GameEngine implements KeyListener, GameReporter{
 	private SpaceShip v;	
 	private Timer timer;
 	private long score = 0;
+	private double difficulty = 0.1;
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 	
 	public GameEngine(GamePanel gp, SpaceShip v) {
 		this.gp = gp;
@@ -36,11 +38,32 @@ public class GameEngine implements KeyListener, GameReporter{
 		timer.start();
 	}
 	
+	private void generateEnemy(){
+		Enemy e = new Enemy((int)(Math.random()*390), 30);
+		gp.sprites.add(e);
+		enemies.add(e);
+	}
+	
 	private void process(){	
+	
+	    if(Math.random() < difficulty){
+			generateEnemy();
+		}
+		
+		Iterator<Enemy> e_iter = enemies.iterator();
+		while(e_iter.hasNext()){
+			Enemy e = e_iter.next();
+			e.proceed();
+			
+		}
+		
 		gp.updateGameUI(this);
 		
 		Rectangle2D.Double vr = v.getRectangle();
 		Rectangle2D.Double er;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+		}
 	}
 	
 	void controlVehicle(KeyEvent e) {
